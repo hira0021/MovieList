fdf5435f370896f3ab48abb4255ce48ddcb37209
hira0021 <arief21.hidayat21@gmail.com>
2023-01-06 10:24:05 +0700

    #6.1 Add ChuckerInterceptor

--- app/src/main/java/com/example/movielist/di/MovieRetrofitModule.kt
+++ app/src/main/java/com/example/movielist/di/MovieRetrofitModule.kt
@@ -1,5 +1,9 @@
 package com.example.movielist.di
 
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.chuckerteam.chucker.api.RetentionManager
 import com.example.movielist.data.remote.MovieService
 import com.example.movielist.util.Const
 import com.google.gson.Gson
@@ -7,6 +11,7 @@ import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -26,9 +31,30 @@ object MovieRetrofitModule {
 
     @Singleton
     @Provides
-    fun provideOkhttpLoggingInterceptor(): OkHttpClient {
+    fun provideChuckerCollector(@ApplicationContext context: Context): ChuckerCollector {
+        return ChuckerCollector(
+            context,
+            showNotification = true,
+            retentionPeriod = RetentionManager.Period.ONE_HOUR
+        )
+    }
+
+    @Singleton
+    @Provides
+    fun provideChuckerInterceptor(@ApplicationContext context: Context, chuckerCollector: ChuckerCollector): ChuckerInterceptor {
+        return ChuckerInterceptor.Builder(context)
+            .collector(chuckerCollector)
+            .maxContentLength(250000L)
+            .redactHeaders(emptySet())
+            .alwaysReadResponseBody(true)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkhttpLoggingInterceptor(chuckerInterceptor: ChuckerInterceptor): OkHttpClient {
         val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-        return OkHttpClient.Builder().addInterceptor(logging).build()
+        return OkHttpClient.Builder().addInterceptor(logging).addInterceptor(chuckerInterceptor).build()
     }
 
     @Singleton
